14425a2a75af7f0b26a71791025c429c1838284d
filiperamon <filiperamon@great.ufc.br>
2015-12-10 08:24:09 -0300

    Funcao audio on/off

--- GameRandomBall/app/src/main/java/wiiber/com/gamerandomball/MainActivity.java
+++ GameRandomBall/app/src/main/java/wiiber/com/gamerandomball/MainActivity.java
@@ -4,29 +4,29 @@ import android.app.Activity;
 import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.TextView;
-
 import java.util.Random;
 
 public class MainActivity extends Activity {
 
-    private Switch switchGerar;
-    private TextView textViewNumberRandom, tvSoma;
+    private Switch switchRest;
+    private TextView textViewNumberRandom, tvSum;
     private Button tvNumOne, tvNumTwo, tvNumTree, tvNumFour, tvNumFive, tvNumSix ;
+    private ImageButton btnVolume;
     public Random random;
     private int count = 0;
-    private int jogada = 0;
-    private int[] resultados;
-    private int soma;
-    private MediaPlayer mediaRoleta;
+    private int player = 0;
+    private int[] results;
+    private int soma, volume;
+    private MediaPlayer audeoRoleta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,11 +40,12 @@ public class MainActivity extends Activity {
         tvNumFour    = (Button) findViewById(R.id.textViewNumFour);
         tvNumFive    = (Button) findViewById(R.id.textViewNumFive);
         tvNumSix     = (Button) findViewById(R.id.textViewNumSix);
-        tvSoma       = (TextView) findViewById(R.id.textViewSoma);
-        switchGerar  = (Switch) findViewById(R.id.switchGerar);
+        btnVolume = (ImageButton) findViewById(R.id.volume);
+        tvSum = (TextView) findViewById(R.id.textViewSoma);
+        switchRest = (Switch) findViewById(R.id.switchGerar);
 
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/postmaster.ttf");
-        tvSoma.setTypeface(font);
+        tvSum.setTypeface(font);
         textViewNumberRandom.setTypeface(font);
         tvNumOne.setTypeface(font);
         tvNumTwo.setTypeface(font);
@@ -52,20 +53,21 @@ public class MainActivity extends Activity {
         tvNumFour.setTypeface(font);
         tvNumFive.setTypeface(font);
         tvNumSix.setTypeface(font);
-        switchGerar.setTypeface(font);
+        switchRest.setTypeface(font);
 
-        mediaRoleta = MediaPlayer.create(this,R.raw.roleta);
+        audeoRoleta = MediaPlayer.create(this,R.raw.roleta);
         random = new Random();
         textViewNumberRandom.setText("?");
-        resultados = new int[6];
+        results = new int[6];
         soma = 0;
+        volume = 1;
 
-        switchGerar.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        switchRest.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    resetarNumeros();
-                    switchGerar.setChecked(false);
+                    resetNumbers();
+                    switchRest.setChecked(false);
                 }
             }
         });
@@ -74,24 +76,39 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 if (count == 0) {
-                    if (jogada <= 6) {
-                        atualizaView();
+                    if (player <= 6) {
+                        refreshView();
                     } else {
-                        jogada = 0;
+                        player = 0;
                         soma = 0;
                     }
+                }
+            }
+        });
+
+        btnVolume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (volume == 1) {
+                    btnVolume.setImageResource(R.drawable.volume_off);
+                    volume = 0;
+                } else {
+                    btnVolume.setImageResource(R.drawable.volume_high);
+                    volume = 1;
                 }
             }
         });
    }
 
     private void efeitoSonoroRoleta(){
-        mediaRoleta.start();
+        if(volume==1)
+            audeoRoleta.start();
     }
 
-    private void atualizaView(){
-        final int numeroGerado = random.nextInt(60) + 1;
-        textViewNumberRandom.setText(String.valueOf(numeroGerado));
+    private void refreshView(){
+        final int numberRandom = random.nextInt(60) + 1;
+        textViewNumberRandom.setText(String.valueOf(numberRandom));
         count ++;
 
         Handler handler = new Handler(getMainLooper());
@@ -100,67 +117,67 @@ public class MainActivity extends Activity {
             public void run() {
                 if (count < 10) {
                     efeitoSonoroRoleta();
-                    animation2(textViewNumberRandom);
-                    atualizaView();
+                    animationRoleta(textViewNumberRandom);
+                    refreshView();
                 } else {
                     count = 0;
-                    jogada ++;
-                    textViewNumberRandom.setText(String.valueOf(numeroGerado));
+                    player++;
+                    textViewNumberRandom.setText(String.valueOf(numberRandom));
 
-                    if(jogada == 6){
+                    if(player == 6){
                         textViewNumberRandom.setClickable(false);
                     }
 
-                    switch (jogada) {
+                    switch (player) {
                         case 1:
-                            animation(tvNumOne);
-                            resultados[0] = Integer.valueOf(textViewNumberRandom.getText().toString());
+                            animationResults(tvNumOne);
+                            results[0] = Integer.valueOf(textViewNumberRandom.getText().toString());
                             animationButton(tvNumOne, textViewNumberRandom.getText().toString());
                             break;
                         case 2:
-                            animation(tvNumTwo);
-                            resultados[0] = Integer.valueOf(textViewNumberRandom.getText().toString());
+                            animationResults(tvNumTwo);
+                            results[0] = Integer.valueOf(textViewNumberRandom.getText().toString());
                             animationButton(tvNumTwo, textViewNumberRandom.getText().toString());
                             break;
                         case 3:
-                            animation(tvNumTree);
-                            resultados[0] = Integer.valueOf(textViewNumberRandom.getText().toString());
+                            animationResults(tvNumTree);
+                            results[0] = Integer.valueOf(textViewNumberRandom.getText().toString());
                             animationButton(tvNumTree, textViewNumberRandom.getText().toString());
                             break;
                         case 4:
-                            animation(tvNumFour);
-                            resultados[0] = Integer.valueOf(textViewNumberRandom.getText().toString());
+                            animationResults(tvNumFour);
+                            results[0] = Integer.valueOf(textViewNumberRandom.getText().toString());
                             animationButton(tvNumFour, textViewNumberRandom.getText().toString());
                             break;
                         case 5:
-                            animation(tvNumFive);
-                            resultados[0] = Integer.valueOf(textViewNumberRandom.getText().toString());
+                            animationResults(tvNumFive);
+                            results[0] = Integer.valueOf(textViewNumberRandom.getText().toString());
                             animationButton(tvNumFive, textViewNumberRandom.getText().toString());
                             break;
                         case 6:
-                            animation(tvNumSix);
-                            resultados[0] = Integer.valueOf(textViewNumberRandom.getText().toString());
+                            animationResults(tvNumSix);
+                            results[0] = Integer.valueOf(textViewNumberRandom.getText().toString());
                             animationButton(tvNumSix, textViewNumberRandom.getText().toString());
-                            jogada = 0;
+                            player = 0;
                             break;
                     }
 
-                    for(int i = 0; i < resultados.length; i++){
-                        soma += resultados[i];
+                    for(int i = 0; i < results.length; i++){
+                        soma += results[i];
                     }
-                    tvSoma.setText(getString(R.string.soma) + ": " + soma);
+                    tvSum.setText(getString(R.string.soma) + ": " + soma);
                 }
             }
         }, 60);
     }
 
-    private void animation(View view) {
+    private void animationResults(View view) {
         Animation animator = AnimationUtils.loadAnimation(this, R.anim.from_middle);
         view.setAnimation(animator);
         view.startAnimation(animator);
     }
 
-    private void animation2(View view) {
+    private void animationRoleta(View view) {
         Animation animator = AnimationUtils.loadAnimation(this, R.anim.from_middle2);
         view.setAnimation(animator);
         view.startAnimation(animator);
@@ -176,29 +193,29 @@ public class MainActivity extends Activity {
         }, 300);
     }
 
-    private void resetarNumeros(){
+    private void resetNumbers(){
 
-        animation(tvNumOne);
+        animationResults(tvNumOne);
         animationButton(tvNumOne, "");
-        animation(tvNumTwo);
+        animationResults(tvNumTwo);
         animationButton(tvNumTwo, "");
-        animation(tvNumTree);
+        animationResults(tvNumTree);
         animationButton(tvNumTree, "");
-        animation(tvNumFour);
+        animationResults(tvNumFour);
         animationButton(tvNumFour, "");
-        animation(tvNumFive);
+        animationResults(tvNumFive);
         animationButton(tvNumFive, "");
-        animation(tvNumSix);
+        animationResults(tvNumSix);
         animationButton(tvNumSix, "");
 
-        animation2(textViewNumberRandom);
+        animationRoleta(textViewNumberRandom);
         animationButton(textViewNumberRandom, "?");
         textViewNumberRandom.setClickable(true);
 
         count = 0;
         soma = 0;
-        tvSoma.setText(getString(R.string.soma) + ": " + soma);
-        resultados = new int[6];
-        jogada = 0;
+        tvSum.setText(getString(R.string.soma) + ": " + soma);
+        results = new int[6];
+        player = 0;
     }
 }
